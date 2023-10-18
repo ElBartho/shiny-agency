6b3401d303ff5ed299f829d8bd1cbe6d55011073
Jérémy Barthelemy <barthelemy.pro@gmail.com>
2023-10-18 10:40:53 +0200

    Results page done

--- src/pages/Results/index.jsx
+++ src/pages/Results/index.jsx
@@ -5,21 +5,69 @@ import colors from '../../utils/style/colors';
 import { StyledLink, Loader } from '../../utils/style/Atoms';
 import { SurveyContext, ThemeContext } from '../../utils/context';
 
+const ResultsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 90px;
+  padding: 30px;
+  background-color: ${({ theme }) =>
+    theme === 'light' ? colors.backgroundLight : colors.backgroundDark};
+`;
+
+const ResultsTitle = styled.h2`
+  color: ${({ theme }) => (theme === 'light' ? '#000000' : '#ffffff')};
+  font-weight: bold;
+  font-size: 28px;
+  max-width: 60%;
+  text-align: center;
+  & > span {
+    padding-left: 10px;
+  }
+`;
+
+const DescriptionWrapper = styled.div`
+  padding: 60px;
+`;
+
+const JobTitle = styled.span`
+  color: ${({ theme }) =>
+    theme === 'light' ? colors.primary : colors.backgroundLight};
+  text-transform: capitalize;
+`;
+
+const JobDescription = styled.div`
+  font-size: 18px;
+  & > p {
+    color: ${({ theme }) => (theme === 'light' ? colors.secondary : '#ffffff')};
+    margin-block-start: 5px;
+  }
+  & > span {
+    font-size: 20px;
+  }
+`;
+
+const LoaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 function formatFetchParams(answers) {
   const answerNumbers = Object.keys(answers);
 
   return answerNumbers.reduce((previousParams, answerNumber, index) => {
     const isFirstAnswer = index === 0;
     const separator = isFirstAnswer ? '' : '&';
+
     return `${previousParams}${separator}a${answerNumber}=${answers[answerNumber]}`;
-  });
+  }, '');
 }
 
 function Results() {
   const { theme } = useContext(ThemeContext);
   const { answers } = useContext(SurveyContext);
   const fetchParams = formatFetchParams(answers);
-
+  console.log('fetchParams+++++++++++++ ', fetchParams);
   const { data, isLoading, error } = useFetch(
     `http://localhost:8000/results?${fetchParams}`
   );
@@ -31,16 +79,47 @@ function Results() {
   }
 
   const { resultsData } = data;
+  const titles =
+    resultsData &&
+    resultsData.reduce((res, element) => {
+      return res === '' ? res + element.title : res + `, ${element.title}`;
+    }, '');
 
   return isLoading ? (
-    <div></div>
+    <LoaderWrapper>
+      <Loader />
+    </LoaderWrapper>
   ) : (
-    <div>
-      <h2>Les compétences dont vous avez besoins: </h2>
+    <ResultsContainer theme={theme}>
+      <ResultsTitle theme={theme}>
+        Les compétences dont vous avez besoin :
+        {resultsData &&
+          resultsData.map((result, index) => (
+            <JobTitle
+              key={`result-title-${index}-${result.title}`}
+              theme={theme}
+            >
+              {result.title}
+              {index === resultsData.length - 1 ? '' : ','}
+            </JobTitle>
+          ))}
+      </ResultsTitle>
       <StyledLink $isFullLink to='/freelances'>
         Découvrez nos profils
       </StyledLink>
-    </div>
+      <DescriptionWrapper>
+        {resultsData &&
+          resultsData.map((result, index) => (
+            <JobDescription
+              theme={theme}
+              key={`result-detail-${index}-${result.title}`}
+            >
+              <JobTitle theme={theme}>{result.title}</JobTitle>
+              <p>{result.description}</p>
+            </JobDescription>
+          ))}
+      </DescriptionWrapper>
+    </ResultsContainer>
   );
 }
 
